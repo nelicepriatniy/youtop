d76fba095c7160013292aa2242a7bbfe0d7afbb1
nelicepriatniy <clavik.polyakov@yandex.ru>
2024-10-03 02:37:33 +0300

    laptop and mobile main page

--- js/app.js
+++ js/app.js
@@ -1,23 +1,26 @@
-//активация хедера
 
-document.addEventListener('DOMContentLoaded', ()=>{
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  //активация хедера
+
   const header = document.querySelector('.header');
   const closeAllPopups = document.querySelector('.closeAllPopups')
   const popups = document.querySelectorAll('.popup')
   const headerMenuBtn = header.querySelector('.header_menu_button');
-  if(header) {
+  if (header) {
     const headerBottomMenuBtn = header.querySelector('.have-child');
     const headerCityInput = header.querySelector('.header_left_select');
-    if(headerMenuBtn) {
-      headerMenuBtn.onclick = ()=>{
+    if (headerMenuBtn) {
+      headerMenuBtn.onclick = () => {
         console.log(123);
-        if(headerMenuBtn.classList.contains('close')) {
+        if (headerMenuBtn.classList.contains('close')) {
           headerMenuBtn.classList.add('open')
           headerMenuBtn.classList.remove('close')
           header.classList.add('active')
           document.querySelector('html').classList.add('noScroll')
           closeAllPopups.classList.add('active')
-        } else if(!headerMenuBtn.classList.contains('close')) {
+        } else if (!headerMenuBtn.classList.contains('close')) {
           headerMenuBtn.classList.remove('open')
           headerMenuBtn.classList.add('close')
           header.classList.remove('active')
@@ -27,31 +30,37 @@ document.addEventListener('DOMContentLoaded', ()=>{
         }
       }
     }
-    if(headerBottomMenuBtn) {
-      const headerBottmMenu = header.querySelector('.header-bottom_bottom');
-      headerBottomMenuBtn.onclick = ()=>{
+    if (headerBottomMenuBtn) {
+      let headerBottmMenu = header.querySelector('.header-bottom_bottom.desk');
+      if (window.innerWidth > 1160) {
+        headerBottmMenu = header.querySelector('.header-bottom_bottom.desk');
+      } else {
+        headerBottmMenu = header.querySelector('.header-bottom_bottom.mob');
+
+      }
+      headerBottomMenuBtn.onclick = () => {
         headerBottmMenu.classList.toggle('active')
         headerBottomMenuBtn.classList.toggle('active')
       }
     }
-    if(headerCityInput) {
+    if (headerCityInput) {
       const headerCityMenu = header.querySelector('.header_left_select_menu')
-      headerCityInput.onclick = ()=>{
+      headerCityInput.onclick = () => {
         headerCityInput.classList.toggle('active')
       }
-      headerCityMenu.addEventListener('mouseleave', ()=>{
+      headerCityMenu.addEventListener('mouseleave', () => {
         headerCityInput.classList.remove('active')
 
       })
-      header.addEventListener('mouseleave', ()=>{
+      header.addEventListener('mouseleave', () => {
         headerCityInput.classList.remove('active')
 
       })
     }
   }
-  if(closeAllPopups) {
-    closeAllPopups.onclick = ()=>{
-      for(let i = 0; popups.length > i; i ++) {
+  if (closeAllPopups) {
+    closeAllPopups.onclick = () => {
+      for (let i = 0; popups.length > i; i++) {
         popups[i].classList.remove('active')
       }
       headerMenuBtn.classList.add('close')
@@ -61,190 +70,313 @@ document.addEventListener('DOMContentLoaded', ()=>{
     }
   }
 
-//работа табов в блоке с табами 
+  //работа табов в блоке с табами 
 
-const tabBlock = document.querySelector('.tabs');
+  const tabBlock = document.querySelector('.tabs');
 
-if(tabBlock) {
-  const tabs = tabBlock.querySelectorAll('.tabs-l-item');
-  const tabsRight = tabBlock.querySelector('.tabs-r');
-  let tabList = `<p class="Text_20">Ошибка</p>`;
-  if(tabs.length > 0) {
-    for(let i = 0; tabs.length > i; i++) {
-      tabs[i].onclick = ()=>{
-        for(let o = 0; tabs.length > o; o++) {
-          tabs[o].classList.remove('active')
-        }
-        tabs[i].classList.add('active')
-
-        tabList = tabs[i].querySelector('.tabs-l-inv-list').cloneNode(true);
-        tabsRight.innerHTML = ''
-        tabsRight.append(tabList)
-      }
-    }
-  }
-}
-
-//работа вопросов
-
-const faqBlock = document.querySelector('.FaQ')
-
-if(faqBlock) {
-  const faqList = faqBlock.querySelectorAll('.faq-item');
-
-  if(faqList.length > 0) {
-    for(let i = 0; faqList.length > i; i++) {
-      faqList[i].onclick = ()=>{
-        faqList[i].classList.toggle('active')
-      }
-    }
-  }
-}
-
-//работа кнопки "показать ещё" в блоке продвижения
-
-const prodvBlock = document.querySelector('.prodv');
-
-if(prodvBlock) {
-  const prodvList = prodvBlock.querySelectorAll('.prodv-item');
-  const prodvBtn = prodvBlock.querySelector('.prov-next-button')
-  if(prodvList.length < 24) {
-    prodvBtn.classList.add('noActive')
-    for(let i = 0; prodvList.length > i; i++) {
-      prodvList[i].classList.add('visual')
-    }
-  } else {
-    let indexVisual = 24;
-    for(let i = 0; indexVisual > i; i++) {
-      prodvList[i].classList.add('visual')
-      prodvBtn.onclick = ()=>{
-        if((indexVisual + 6) < prodvList.length) {
-          indexVisual = indexVisual + 6;
-          for(let o = 0; indexVisual > o; o++ ) {
-            prodvList[o].classList.add('visual')
+  if (tabBlock) {
+    const tabs = tabBlock.querySelectorAll('.tabs-l-item');
+    const tabsRight = tabBlock.querySelector('.tabs-r');
+    let tabList = `<p class="Text_20">Ошибка</p>`;
+    if (tabs.length > 0) {
+      for (let i = 0; tabs.length > i; i++) {
+        tabs[i].onclick = () => {
+          for (let o = 0; tabs.length > o; o++) {
+            tabs[o].classList.remove('active')
           }
-        } else {
-          indexVisual = indexVisual + (prodvList.length - indexVisual);
-          for(let o = 0; indexVisual > o; o++ ) {
-            prodvList[o].classList.add('visual')
-          }
-          prodvBtn.classList.add('noActive')
+          tabs[i].classList.add('active')
+
+          tabList = tabs[i].querySelector('.tabs-l-inv-list').cloneNode(true);
+          tabsRight.innerHTML = ''
+          tabsRight.append(tabList)
         }
       }
     }
   }
-}
 
-//работа кнопки "показать ещё" в блоке видео
+  //работа вопросов
 
-const videosBlock = document.querySelector('.videos');
+  const faqBlock = document.querySelector('.FaQ')
 
-if(prodvBlock) {
-  const videosList = videosBlock.querySelectorAll('.video-item');
-  const videosBtn = videosBlock.querySelector('.prov-next-button')
-  if(videosList.length < 6) {
-    videosBtn.classList.add('noActive')
-    for(let i = 0; videosList.length > i; i++) {
-      videosList[i].classList.add('active')
-    }
-  } else {
-    let indexVisual = 6;
-    for(let i = 0; indexVisual > i; i++) {
-      videosList[i].classList.add('active')
-      videosBtn.onclick = ()=>{
-        if((indexVisual + 3) < videosList.length) {
-          indexVisual = indexVisual + 3;
-          for(let o = 0; indexVisual > o; o++ ) {
-            videosList[o].classList.add('active')
-          }
-        } else {
-          indexVisual = indexVisual + (videosList.length - indexVisual);
-          for(let o = 0; indexVisual > o; o++ ) {
-            videosList[o].classList.add('active')
-          }
-          videosBtn.classList.add('noActive')
+  if (faqBlock) {
+    const faqList = faqBlock.querySelectorAll('.faq-item');
+
+    if (faqList.length > 0) {
+      for (let i = 0; faqList.length > i; i++) {
+        faqList[i].onclick = () => {
+          faqList[i].classList.toggle('active')
         }
       }
     }
   }
-}
 
-//работа скрытого меню в футере
+  //работа кнопки "показать ещё" в блоке продвижения
 
-const footer = document.querySelector('.footer')
+  const prodvBlock = document.querySelector('.prodv');
 
-if(footer) {
-  const footerBtn = footer.querySelector('.footer-menu-item.more')
-  const footerHiddenMenu = footer.querySelector('.footer-hidden')
-  footerBtn.onclick = ()=>{
-    footerHiddenMenu.classList.toggle('active')
-    footerBtn.classList.toggle('active')
-  }
-
-}
-
-//вызов модалки с формой
-
-const modalForm = document.querySelector('.form-popup')
-
-if(modalForm) {
-  const formModalBtns = document.querySelectorAll('.form-btn')
-  const formModalClose = modalForm.querySelector('.form-poopup-close-svg')
-  for(let i = 0; formModalBtns.length > i; i++) {
-    formModalBtns[i].onclick =()=>{
-      modalForm.classList.add('active')
-      closeAllPopups.classList.add('active')
-      document.querySelector('html').classList.add('noScroll')
+  if (prodvBlock) {
+    const prodvList = prodvBlock.querySelectorAll('.prodv-item');
+    const prodvBtn = prodvBlock.querySelector('.prov-next-button');
+    let indexToMove = 6,
+        startIndex = 24;
+    if(window.innerWidth < 1160 && window.innerWidth > 640) {
+      indexToMove = 4;
+      startIndex = 16;
+    } else if(window.innerWidth < 640) {
+      indexToMove = 4;
+      startIndex = 8;
+    }
+    if (prodvList.length < startIndex) {
+      prodvBtn.classList.add('noActive')
+      for (let i = 0; prodvList.length > i; i++) {
+        prodvList[i].classList.add('visual')
+      }
+    } else {
+      let indexVisual = startIndex;
+      for (let i = 0; indexVisual > i; i++) {
+        prodvList[i].classList.add('visual')
+        prodvBtn.onclick = () => {
+          if ((indexVisual + indexToMove) < prodvList.length) {
+            indexVisual = indexVisual + indexToMove;
+            for (let o = 0; indexVisual > o; o++) {
+              prodvList[o].classList.add('visual')
+            }
+          } else {
+            indexVisual = indexVisual + (prodvList.length - indexVisual);
+            for (let o = 0; indexVisual > o; o++) {
+              prodvList[o].classList.add('visual')
+            }
+            prodvBtn.classList.add('noActive')
+          }
+        }
+      }
     }
   }
-  formModalClose.onclick = ()=>{
-    modalForm.classList.remove('active')
-    closeAllPopups.classList.remove('active')
-    document.querySelector('html').classList.remove('noScroll')
+
+  //работа кнопки "показать ещё" в блоке видео
+
+  const videosBlock = document.querySelector('.videos');
+
+  if (prodvBlock) {
+    const videosList = videosBlock.querySelectorAll('.video-item');
+    const videosBtn = videosBlock.querySelector('.prov-next-button')
+    let indexToMove = 3,
+        startIndex = 6;
+    if(window.innerWidth < 1160) {
+      indexToMove = 2;
+      startIndex = 4;
+    } else {
+      indexToMove = 3;
+      startIndex = 6;
+    }
+    if (videosList.length < startIndex) {
+      videosBtn.classList.add('noActive')
+      for (let i = 0; videosList.length > i; i++) {
+        videosList[i].classList.add('active')
+      }
+    } else {
+      let indexVisual = startIndex;
+      for (let i = 0; indexVisual > i; i++) {
+        videosList[i].classList.add('active')
+        videosBtn.onclick = () => {
+          if ((indexVisual + indexToMove) < videosList.length) {
+            indexVisual = indexVisual + indexToMove;
+            for (let o = 0; indexVisual > o; o++) {
+              videosList[o].classList.add('active')
+            }
+          } else {
+            indexVisual = indexVisual + (videosList.length - indexVisual);
+            for (let o = 0; indexVisual > o; o++) {
+              videosList[o].classList.add('active')
+            }
+            videosBtn.classList.add('noActive')
+          }
+        }
+      }
+    }
+  }
+
+  //работа скрытого меню в футере
+
+  const footer = document.querySelector('.footer')
+
+  if (footer) {
+    const footerBtn = footer.querySelector('.footer-menu-item.more')
+    let footerHiddenMenu = footer.querySelector('.footer-hidden.desk')
+    if(window.innerWidth < 640) {
+      footerHiddenMenu = footer.querySelector('.footer-hidden.mob-s')
+    }
+    footerBtn.onclick = () => {
+      footerHiddenMenu.classList.toggle('active')
+      footerBtn.classList.toggle('active')
+    }
 
   }
-}
 
-//слайдер кейсов слайдера 
+  //вызов модалки с формой
 
-const keysSlider = new Swiper('.ya-keys-swiper', {
-  speed: 400,
-  spaceBetween: 100,
-  slidesPerView: 1,
-  navigation: {
-    nextEl: '.ya-keys-next',
-    prevEl: '.ya-keys-prev'
+  const modalForm = document.querySelector('.form-popup')
+
+  if (modalForm) {
+    const formModalBtns = document.querySelectorAll('.form-btn')
+    const formModalClose = modalForm.querySelector('.form-poopup-close-svg')
+    for (let i = 0; formModalBtns.length > i; i++) {
+      formModalBtns[i].onclick = () => {
+        modalForm.classList.add('active')
+        closeAllPopups.classList.add('active')
+        document.querySelector('html').classList.add('noScroll')
+      }
+    }
+    formModalClose.onclick = () => {
+      modalForm.classList.remove('active')
+      closeAllPopups.classList.remove('active')
+      document.querySelector('html').classList.remove('noScroll')
+
+    }
   }
-});
 
-//слайдер калькулятора квиза
+  //мобилка 
 
-const calkSlider = new Swiper('.calkulator-swiper', {
-  speed: 400,
-  spaceBetween: 100,
-  slidesPerView: 1,
-  allowTouchMove: false,
-  autoHeight: true,
-  navigation: {
-    nextEl: '.calk-left-next',
-  },
-  pagination: {
-    el: '.calk-left-pag'
+  if(window.innerWidth < 640) {
+
+    // раскрытие текста в about me
+
+    const readMoreBtn = document.querySelector('.about-me .read-next')
+    readMoreBtn.onclick = ()=>{
+      document.querySelector('.about-me_info_heading.mobile').classList.toggle('more')
+      readMoreBtn.classList.toggle('open')
+      if(!readMoreBtn.classList.contains('open')) {
+        readMoreBtn.querySelector('.read-next-text').innerHTML = 'Читать дальше'
+      } else {
+        readMoreBtn.querySelector('.read-next-text').innerHTML = 'Свернуть'
+      }
+    }
+
+    const yaKekyses = document.querySelectorAll('.ya-keys-item');
+
+    if(yaKekyses.length > 0) {
+      for(let i = 0; yaKekyses.length > i; i++) {
+        let yaTabs = yaKekyses[i].querySelectorAll('.ya-keyses-tabs-tab');
+        yaKekyses[i].querySelector('.keys-item-left__positons').classList.add('active')
+        yaKekyses[i].querySelector('.keys-item-left__zaprosi').classList.remove('active')
+        yaKekyses[i].querySelector('.keys-item__left__top-ya.mob').classList.remove('active')
+        for(let o = 0; yaTabs.length > o; o++) {
+          yaTabs[o].onclick = ()=>{
+            for(let j = 0; yaTabs.length > j; j++) {
+              yaTabs[j].classList.remove('active')
+
+            }
+            let yaTabAttr = yaTabs[o].getAttribute('data-tab')
+            yaTabs[o].classList.add('active')
+            if(yaTabAttr == 'result ' ) {
+              yaKekyses[i].querySelector('.keys-item-left__positons').classList.add('active')
+              yaKekyses[i].querySelector('.keys-item-left__zaprosi').classList.remove('active')
+              yaKekyses[i].querySelector('.keys-item__left__top-ya.mob').classList.remove('active')
+            } else {
+              yaKekyses[i].querySelector('.keys-item-left__positons').classList.remove('active')
+              yaKekyses[i].querySelector('.keys-item-left__zaprosi').classList.add('active')
+              yaKekyses[i].querySelector('.keys-item__left__top-ya.mob').classList.add('active')
+
+            }
+          }
+        }
+      }
+    }
   }
-});
 
-//слайдер видеоотзывов
+  //слайдер кейсов слайдера 
 
-const videoRevSlider = new Swiper('.video-rev-slider', {
-  speed: 400,
-  spaceBetween: 40,
-  slidesPerView: 3,
-  navigation: {
-    nextEl: '.video-arr-next',
-    prevEl: '.video-arr-prev'
-  },
-  pagination: {
-    el: '.video-rev-paggination'
+  const keysSlider = new Swiper('.ya-keys-swiper', {
+    speed: 400,
+    spaceBetween: 100,
+    slidesPerView: 1,
+    navigation: {
+      nextEl: '.ya-keys-next.desk',
+      prevEl: '.ya-keys-prev.desk'
+    },
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 100,
+        navigation: {
+          nextEl: '.ya-keys-next.mob',
+          prevEl: '.ya-keys-prev.mob'
+        },
+      },
+      // when window width is >= 480px
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 100,
+        navigation: {
+          nextEl: '.ya-keys-next.mob',
+          prevEl: '.ya-keys-prev.mob'
+        },
+      },
+      // when window width is >= 640px
+      1160: {
+        slidesPerView: 1,
+        spaceBetween: 100
+      }
+    },
+  });
+
+  if(window.innerWidth < 1160) {
+    const nowYaKeys = document.querySelector('.schet-now-ya-keys')
+    const allYaKeys = document.querySelector('.schet-all-ya-keys')
+    const allYaKeyses = document.querySelectorAll('.ya-keys-swiper .swiper-slide')
+    allYaKeys.innerHTML = allYaKeyses.length
+    keysSlider.on('slideChangeTransitionStart', ()=>{
+      nowYaKeys.innerHTML = keysSlider.activeIndex + 1
+    })
+    keysSlider.init();
   }
-});
+
+  //слайдер калькулятора квиза
+
+  const calkSlider = new Swiper('.calkulator-swiper', {
+    speed: 400,
+    spaceBetween: 100,
+    slidesPerView: 1,
+    allowTouchMove: false,
+    autoHeight: true,
+    navigation: {
+      nextEl: '.calk-left-next',
+    },
+    pagination: {
+      el: '.calk-left-pag'
+    }
+  });
+
+  //слайдер видеоотзывов
+
+  const videoRevSlider = new Swiper('.video-rev-slider', {
+    speed: 400,
+    spaceBetween: 40,
+    slidesPerView: 3,
+    navigation: {
+      nextEl: '.video-arr-next',
+      prevEl: '.video-arr-prev'
+    },
+    pagination: {
+      el: '.video-rev-paggination'
+    },
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+      // when window width is >= 480px
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 30
+      },
+      // when window width is >= 640px
+      1160: {
+        slidesPerView: 3,
+        spaceBetween: 40
+      }
+    }
+  });
 })
